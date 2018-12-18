c1759519d6829b5b5a035aa575fe6a31aa1d7443
mshapir <mshapir95@gmail.com>
2018-12-18 18:15:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,65 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
   fetch('http://localhost:3000/pups')
   .then(res => res.json())
-  .then(renderPups)
-  const pupCollection = document.getElementById('dog-info')
+  .then(pupBar);
+  const pupSpan = document.getElementById('dog-bar')
+  pupSpan.addEventListener('click', toggleData)
+  const pupInfoDiv = document.getElementById('dog-info')
+  pupInfoDiv.addEventListener('click', toggleGoodOrBad)
 })
 
-
-
-function renderPups(pupsData) {
-  pupsData.forEach(pup =>{
-    const pupCollection = document.getElementById('dog-info')
-    const div = document.createElement('div')
-    div.dataset.id = pup.id
-    const header = document.createElement('h2')
-    const image = document.createElement('img')
-    const button = document.createElement('button')
-    header.innerText = pup.name
-    image.src = pup.image
-    if (pup.isGoodDog === true) {
-      button.innerText = 'Good Dog'
-    }else {
-      button.innerText = 'Bad Dog'
-    }
-    div.appendChild(header)
-    div.appendChild(image)
-    div.appendChild(button)
-    pupCollection.appendChild(div)
+function toggleGoodOrBad(event) {
+  const dogId = event.target.dataset.id
+  let isGoodDogValue = event.target.innerText === "Good Dog" ? true : false
+  // event.target.innerText = !isGoodDogValue ? 'Good Dog' : 'Bad Dog'
+  fetch(`http://localhost:3000/pups/${ dogId }`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      isGoodDog: !isGoodDogValue
+    })
   })
+  .then(res => res.json())
+  .then(pupsInfo)
+}
+
+function pupBar(pupsData) {
+  const pupSpan = document.getElementById('dog-bar')
+  pupsData.forEach(pup => {
+    const span = document.createElement('span')
+    span.dataset.id = pup.id
+    span.innerText = pup.name
+    pupSpan.appendChild(span)
+  })
+}
+
+function toggleData(event) {
+  if (event.target.tagName === 'SPAN') {
+    const dogId = event.target.dataset.id
+    fetch(`http://localhost:3000/pups/${ dogId }`)
+    .then(res => res.json())
+    .then(pupsInfo);
+  }
+}
+
+function pupsInfo(pupData) {
+  const pupCollection = document.getElementById('dog-info')
+  pupCollection.innerHTML = ''
+  const image = document.createElement('img')
+  const header = document.createElement('h2')
+  const button = document.createElement('button')
+  button.dataset.id = pupData.id
+  header.innerText = pupData.name
+  image.src = pupData.image
+  if (pupData.isGoodDog === true) {
+    button.innerText = 'Good Dog'
+  }else {
+    button.innerText = 'Bad Dog'
+  }
+  pupCollection.appendChild(header)
+  pupCollection.appendChild(image)
+  pupCollection.appendChild(button)
+
 }
